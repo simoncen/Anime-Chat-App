e606f12d8047686df62bcf1673109d0dac49ea04
Jin Lin Cen <jinlincen@JinLins-MacBook-Pro.local>
2021-08-11 12:37:55 +0800

    LoginForm Text changed

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -32,7 +32,7 @@ const LoginForm = () => {
     return (
         <div className="wrapper">
             <div className="form">
-                <h1 className="title">Chat Application</h1>
+                <h1 className="title">Anime Chat App</h1>
                 <form onSubmit={handleSubmit}>
                     <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required />  {/* required means we need to type in that user */}
                     <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />  
